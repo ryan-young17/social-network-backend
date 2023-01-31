3771e08a3d3b6d637ac25850119dc52e9f4a41e5
Ryan Young <ryanbyoung1@gmail.com>
2023-01-31 15:34:37 -0500

    added thought model

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
